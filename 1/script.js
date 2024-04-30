f147b0f761dcd4d4378471b1f6566f0eaf41d4e1
Geovana Matos <geovanabmatos@gmail.com>
2024-04-29 22:04:57 -0300

    questão 1

--- 1/script.js
+++ 1/script.js
@@ -1,3 +1,16 @@
+let altura= parseFloat(prompt("Digite a sua altura:"));
+let peso= parseFloat(prompt("Digite o seu peso:"));
 
+let imc=(peso/(altura*altura));
+
+//document.write("Seu IMC: "+ imc);
+let classeimc=
+(imc<18.5) ? "Abaixo do peso normal":
+(imc>=18.5 && imc<=24.9) ? "Peso normal":
+(imc>=25 && imc<=29.9) ? "Excesso de peso":
+(imc>=30 && imc<=34.9) ? "Obesidade classe I":
+(imc>=35 && imc<=39.9) ? "Obesidade classe II":"Obesidade classe III";
+
+document.write(classeimc);
 
 
